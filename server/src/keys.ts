2e9949d01957a2c14f977c5b64687e4665fafcb9
yayagabay <yairgabbay99@gmail.com>
2019-01-20 07:49:34 +0200

    Update keys.ts

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -2,9 +2,13 @@ export default{
 
     
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: 'qwe123',
-        database: 'ng_movies_db'
+            host: 'shifter.co.il',
+            user: 'shifter_yair',
+            password: 'vNV?gQV9YPV&',
+            database: 'shifter_mymovies'
+        // host: 'localhost',
+        // user: 'root',
+        // password: 'qwe123',
+        // database: 'ng_movies_db'
     }
 }
