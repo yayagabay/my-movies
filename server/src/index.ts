2b96ad88562a425e3715d5784857191b6c81f823
yayagabay <yairgabbay99@gmail.com>
2019-01-20 09:35:21 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,9 +31,22 @@ class Server {
     }
 
     start(): void{
-        this.app.listen(process.env.PORT || 5000), () =>{
-            console.log('Server on Port', this.app.get('port'));
-        };
+        // this.app.listen(process.env.PORT || 5000), () =>{
+        //     console.log('Server on Port', this.app.get('port'));
+        // };
+        var app     = express();
+        var express = require('express');
+        app.set('port', (process.env.PORT || 5000));
+        //For avoidong Heroku $PORT error
+        app.get('/', function(request, response) {
+        var result = 'App is running'
+        response.send(result);
+        }).listen(app.get('port'), function() {
+        console.log('App is running, server is listening on port ', app.get('port'));
+        });
+
+
+
     }
 
 }
