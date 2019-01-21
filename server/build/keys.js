47859b70ee5ed61e61cde751e64b62433d5ce93a
yayagabay <yairgabbay99@gmail.com>
2019-01-21 18:52:32 +0200

    update

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        // host: 'shifter.co.il',
-        // user: 'shifterc_yair',
-        // password: 'L,6RHgeJr!8x',
-        // database: 'shifterc_mymovies'
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'qwe123',
-        database: 'ng_movies_db'
+        host: 'shifter.co.il',
+        user: 'shifterc_yair',
+        password: 'L,6RHgeJr!8x',
+        database: 'shifterc_mymovies'
+        // host: '127.0.0.1',
+        // user: 'root',
+        // password: 'qwe123',
+        // database: 'ng_movies_db'
     }
 };
