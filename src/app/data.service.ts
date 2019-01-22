78dcc586cfc157e9f59404be6ee109fc878b5415
yayagabay <yairgabbay99@gmail.com>
2019-01-22 17:40:45 +0200

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -56,7 +56,7 @@ saveMovie(string,id) {
   this.checkExist(id);
    let m:Movie = <Movie>JSON.stringify(string);
     console.log(string);
- this.movie.Title=string.Title;
+    this.movie.Title=string.Title.replace(/[^a-zA-Z ]/g, "");
     this.movie.Year=string.Year;
     this.movie.Runtime=string.Runtime;
     this.movie.Genre=string.Genre;
