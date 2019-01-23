106cc18a3b42192016176ef5f42430ff0bf3ebbb
yayagabay <yairgabbay99@gmail.com>
2019-01-23 20:03:55 +0200

    Update movie-search.component.ts

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -43,11 +43,11 @@ export class MovieSearchComponent implements OnInit {
   addMovie(id){
     console.log(this.dataService.checkExist(id));
 
-    this.dataService.getMovieFromOmdb(id).subscribe(
-      res => {
-        this.dataService.saveMovie(res,id);
-          }
+    // this.dataService.getMovieFromOmdb(id).subscribe(
+      // res => {
+      //   this.dataService.saveMovie(res,id);
+      //     }
 
-        )
+        // )
       }   
     }
